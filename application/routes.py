c2a81f293ffc7e0d357fcbfff871379641e6b9b5
Samson Adeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-05 18:25:46 +0000

    Can add new users to the database

--- application/routes.py
+++ application/routes.py
@@ -3,3 +3,22 @@ from flask_login import login_user, current_user, logout_user, login_required
 from application import app, db, bcrypt, login_manager
 from application.models import User, Anime
 from application.forms import RegisterForm, LoginForm
+
+@app.route('/')
+
+@app.route('/register' methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data)
+        user = User(firstname=form.firstname.data, lastname=form.lastname.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+
+@app.route('/home')
+
+@app.route('/logout')
