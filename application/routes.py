2668c4a0700b6e4777d94e4887a3aa33a9c562ff
Samson Adeyinka <samsonadeyinka@hotmail.co.uk>
2019-12-06 13:12:50 +0000

    register function working

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
-from application import app, db, login_manager
+from application import app, db, login_manager, bcrypt
 from application.models import User, Anime
 from application.forms import RegisterForm, LoginForm
 
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -17,5 +20,25 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-# @app.route('/login')
-# @app.route('/home')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html', title='Logout')
